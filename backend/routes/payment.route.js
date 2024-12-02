585706340b4d8c7ba90e673c4c3ffea1635f4c21
rufus-marigi <rufusmarigik@gmail.com>
2024-12-02 22:44:14 +0300

    Create payment.route.js

---
+++ backend/routes/payment.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { createCheckoutSession } from "../controllers/payment.controller.js";
+
+const router = express.Router();
+
+// Define routes here
+router.post("/create-checkout-session", protectRoute, createCheckoutSession);
+router.post("/checkout-success", protectRoute);
+
+export default router;
