a081dc7f096a2990650c7ba34d08ea25451dee57
rufus-marigi <rufusmarigik@gmail.com>
2024-12-11 21:58:37 +0300

    cart function

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
       required: [true, "Password is required"],
       minlength: [6, "Password must be at least 6 characters"],
     },
-    cartItem: [
+    cartItems: [
       {
         product: {
           type: mongoose.Schema.Types.ObjectId,
