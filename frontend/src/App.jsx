5017f9fa6e1fc2c0f6a92c69dafbb3a8d8aeafe0
rufus-marigi <rufusmarigik@gmail.com>
2024-12-16 17:03:51 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,24 +4,32 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
-
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
 import useUserStore from "./stores/useUserStore";
 import { useEffect } from "react";
 import LoadingSpinner from "./components/loadingSpinner";
+import CartPage from "./pages/CartPage";
+import { useCartStore } from "./stores/useCartStore";
 
 function App() {
   const { user, checkAuth, checkingAuth } = useUserStore();
+  const { getCartItems } = useCartStore();
 
   useEffect(() => {
+    // Check authentication status on app load
     checkAuth();
   }, [checkAuth]);
+
+  useEffect(() => {
+    if (!user) return;
+    getCartItems();
+  }, [user, getCartItems]); // Re-run if the `user` changes
+
   if (checkingAuth) return <LoadingSpinner />;
 
   return (
     <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
-      {/* Background gradient */}
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute inset-0">
           <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
@@ -29,7 +37,7 @@ function App() {
       </div>
 
       <div className="relative z-50 pt-20">
-        <Navbar />
+        <Navbar /> {/* Make sure the Navbar reflects the updated cart state */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route
@@ -46,8 +54,11 @@ function App() {
               user?.role === "admin" ? <AdminPage /> : <Navigate to="/login" />
             }
           />
-          {/* categories */}
           <Route path="/category/:category" element={<CategoryPage />} />
+          <Route
+            path="/cart"
+            element={user ? <CartPage /> : <Navigate to="/login" />}
+          />
         </Routes>
       </div>
       <Toaster />
