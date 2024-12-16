f6e3c529b6a3c0a2e192c8e41d412ed2103e1d2a
rufus-marigi <rufusmarigik@gmail.com>
2024-12-16 17:03:35 +0300

    Update cart.route.js

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -5,18 +5,13 @@ import {
   removeAllFromCart,
   updateQuantity,
 } from "../controllers/cart.controller.js";
-
-// import protectRoute middleware
-
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// route to add product to cart
-
 router.get("/", protectRoute, getCartProducts);
 router.post("/", protectRoute, addToCart);
-router.delete("/", protectRoute, removeAllFromCart); //remove all from cart
-router.put("/:id", protectRoute, updateQuantity); //update quantity with new quantity value
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
 
 export default router;
