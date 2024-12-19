a2b67a736c5d6c9ff05661d11e0c3cb8b2c92c74
rufus-marigi <rufusmarigik@gmail.com>
2024-12-19 19:41:01 +0300

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import CategoryItem from "../components/CategoryItem";
+import { useProductStore } from "../stores/useProductStore";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 const categories = [
   { href: "/groceries", name: "Groceries", imageUrl: "/groceries.jpg" },
@@ -47,6 +50,12 @@ const categories = [
 ];
 
 const HomePage = () => {
+  const { fetchFeaturedProducts, products, isLoading } = useProductStore();
+
+  useEffect(() => {
+    fetchFeaturedProducts();
+  }, [fetchFeaturedProducts]);
+
   return (
     <div className="relative min-h-screen text-white overflow-hidden">
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
@@ -54,16 +63,20 @@ const HomePage = () => {
           Explore Our Categories
         </h1>
         <p className="text-center text-xl text-gray-300 mb-12">
-          Discover everything you need, from groceries to household essentials.
+          Discover the latest trends in eco-friendly fashion
         </p>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {categories.map((category) => (
-            <CategoryItem key={category.name} category={category} />
+            <CategoryItem category={category} key={category.name} />
           ))}
         </div>
+
+        {!isLoading && products.length > 0 && (
+          <FeaturedProducts featuredProducts={products} />
+        )}
       </div>
     </div>
   );
 };
-
 export default HomePage;
